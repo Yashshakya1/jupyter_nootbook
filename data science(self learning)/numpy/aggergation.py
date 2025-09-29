bf225bb5862367d3a773e67dba992c14f7cc67b6
Yashshakya1 <yashshakya787@gmail.com>
2025-09-30 03:09:12 +0530

    aggergation

---
+++ data science(self learning)/numpy/aggergation.py
@@ -0,0 +1,12 @@
+import numpy as np 
+arr = np.array([[1,2,3,4,5],[1,2,3,4,5]])
+print(np.sum(arr))
+print(np.mean(arr))
+print(np.max(arr,axis = 1))
+print(np.min(arr))
+print(np.std(arr))
+# np.var(): Computes the variance, which is the square of the standard deviation.
+print(np.var(arr))  # μ=1/n ∑^n/i=1  xi
+
+
+
