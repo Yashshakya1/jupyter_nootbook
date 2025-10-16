ce8179d4b569816efa7a1771fca5902b83f45edd
Yashshakya1 <yashshakya787@gmail.com>
2025-10-16 12:17:30 +0530

    some update

---
+++ data science(self learning)/matplotlib/plot.py
@@ -0,0 +1,7 @@
+# creating a line graphs using the plot method  
+import matplotlib.pyplot as plt 
+import numpy as np 
+x_axis = np.array([0,10])
+y_axis = np.array([0,100])
+plt.plot(x_axis,y_axis)
+plt.show()
